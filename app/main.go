73e96579e19432791a88b7fedd93be9618492de8
BuzzingTaz <catzfgnam@gmail.com>
2025-11-25 21:10:56 +0530

    run executables in path

--- app/main.go
+++ app/main.go
@@ -8,10 +8,16 @@ import (
 	"strings"
 )
 
+const (
+	_exitBuiltin = iota
+	_echoBuiltin
+	_typeBuiltin
+)
+
 var shellBuiltins = map[string]int{
-	"exit": 0,
-	"echo": 1,
-	"type": 2,
+	"exit": _exitBuiltin,
+	"echo": _echoBuiltin,
+	"type": _typeBuiltin,
 }
 
 func main() {
@@ -30,32 +36,50 @@ func printPrompt() {
 }
 
 func handleInput() (exit int) {
+	exit = 0
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		return 0
+		return exit
 	}
 
 	command = command[:len(command)-1]
 	commandWords := strings.Split(command, " ")
 
-	switch commandWords[0] {
-	case "exit":
-		return 1
-	case "echo":
-		fmt.Println(strings.Join(commandWords[1:], " "))
-	case "type":
-		if _, ok := shellBuiltins[commandWords[1]]; ok {
-			fmt.Printf("%s is a shell builtin\n", commandWords[1])
-		} else if path, err := findExecutableInPath(commandWords[1]); err == nil {
-			fmt.Printf("%s is %s\n", commandWords[1], path)
-		} else {
-			fmt.Printf("%s: not found\n", commandWords[1])
+	shellBuiltin, isShellBuiltin := shellBuiltins[commandWords[0]]
+	if isShellBuiltin {
+		switch shellBuiltin {
+		case _exitBuiltin:
+			exit = 1
+
+		case _echoBuiltin:
+			fmt.Println(strings.Join(commandWords[1:], " "))
+
+		case _typeBuiltin:
+			if _, ok := shellBuiltins[commandWords[1]]; ok {
+				fmt.Printf("%s is a shell builtin\n", commandWords[1])
+			} else if path, err := findExecutableInPath(commandWords[1]); err == nil {
+				fmt.Printf("%s is %s\n", commandWords[1], path)
+			} else {
+				fmt.Printf("%s: not found\n", commandWords[1])
+			}
 		}
-	default:
-		fmt.Printf("%v: command not found\n", command)
+		return exit
 	}
-	return 0
+
+	path, err := findExecutableInPath(commandWords[0])
+	if err != nil {
+		fmt.Printf("%v: command not found\n", command)
+		return exit
+	}
+
+	cmd := exec.Command(path, commandWords[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	return exit
 }
 
 func findExecutableInPath(cmd string) (string, error) {
